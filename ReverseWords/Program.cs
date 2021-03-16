daa8e360cfb16fc2405d0e8f10f9c8162246c332
Федерягин Андрей <federyagin_an@admsurgut.ru>
2021-03-16 11:19:02 +0500

    ReverseWords problem

---
+++ ReverseWords/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+/// <summary>
+/// Complete the fuction that accepts a string parameter, and reverses each word in the string.
+/// All spaces in the string should be reatained.
+/// Examples: 
+/// "This is an example" ===> "sihT si na !elpmaxe"
+/// "double  spaces" ===> "elbuod  secaps"
+/// </summary>
+namespace ReverseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    public class ReverseWordsApp
+    {
+        public string ReverseWords(string str)
+        {
+            StringBuilder reverseSb = new StringBuilder();
+            string bufer = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != ' ')
+                {
+                    bufer += str[i];
+                }
+
+                if (str[i] == ' ')
+                {
+                    if (string.IsNullOrWhiteSpace(bufer) == false)
+                        reverseSb.Append(ReverseOneWord(bufer));
+
+                    bufer = "";
+                    reverseSb.Append(" ");
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(bufer) == false)
+                reverseSb.Append(ReverseOneWord(bufer));
+
+            return reverseSb.ToString();
+        }
+
+        private string ReverseOneWord(string word)
+        {
+            StringBuilder reverseSb = new StringBuilder();
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reverseSb.Append(word[i]);
+            }
+            return reverseSb.ToString();
+        }
+    }
+}
---
+++ TestProject/ReverseWordsAppTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using ReverseWords;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    class ReverseWordsAppTests
+    {
+        [Test]
+        [TestCase("This is an example!", "sihT si na !elpmaxe")]
+        [TestCase("double  spaces", "elbuod  secaps")]
+        [TestCase(" example ", " elpmaxe ")]
+        public void ReverseWords_ShouldReturnTrue(string str, string expectedResult)
+        {
+            var result = new ReverseWordsApp().ReverseWords(str);
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
