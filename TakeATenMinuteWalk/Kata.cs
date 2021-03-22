36405a735428bebb7d38af9f0f29942afa5e13dc
Федерягин Андрей <federyagin_an@admsurgut.ru>
2021-03-22 11:01:23 +0500

    Take a Ten Minute Walk

---
+++ TakeATenMinuteWalk/Kata.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TakeATenMinuteWalk
+{
+    public class Kata
+    {
+        public bool IsValidWalk(string[] walk)
+        {
+            if (walk.Length != 10)
+                return false;
+
+            int nsWeight = 0;
+            int weWeight = 0;
+
+            for (int i = 0; i < walk.Length; i++)
+            {
+                switch (walk[i])
+                {
+                    case "n":
+                        nsWeight += 1;
+                        break;
+                    case "s":
+                        nsWeight -= 1;
+                        break;
+                    case "w":
+                        weWeight += 1;
+                        break;
+                    case "e":
+                        weWeight -= 1;
+                        break;
+                }
+                
+            }
+
+            if ((nsWeight == 0) && (weWeight == 0))
+                return true;
+            else 
+                return false;
+
+        }
+    }
+}
---
+++ TestProject/TakeATenMinuteWalkTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TakeATenMinuteWalk;
+
+namespace Tests
+{
+    class TakeATenMinuteWalkTest
+    {
+        [Test]
+        public void IsValidWalk_returnTrue()
+        {
+            //Assert.AreEqual(new Kata().IsValidWalk(walk), true);
+
+
+            Assert.AreEqual(true, new Kata().IsValidWalk(new string[] { "n", "s", "n", "s", "n", "s", "n", "s", "n", "s" }));
+            Assert.AreEqual(false, new Kata().IsValidWalk(new string[] { "w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e" }));
+            Assert.AreEqual(false, new Kata().IsValidWalk(new string[] { "w" }));
+            Assert.AreEqual(false, new Kata().IsValidWalk(new string[] { "n", "n", "n", "s", "n", "s", "n", "s", "n", "s" }));
+        }
+        /*
+        [Test]
+        [TestCase()]
+        public void IsValidWalk_returnFalse(string[] walk)
+        {
+            Assert.AreEqual(new Kata().IsValidWalk(walk), false);
+        }*/
+    }
+}
