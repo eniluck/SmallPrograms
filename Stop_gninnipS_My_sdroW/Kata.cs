912131c13abc3b8a6c66b077ade47d8b9381e22a
Eniluck <eniluck@mail.ru>
2021-03-25 15:03:10 +0500

    Stop_gninnipS_My_sdroW

---
+++ Stop_gninnipS_My_sdroW/Kata.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Stop_gninnipS_My_sdroW
+{
+    public class Kata
+    {
+        public string SpinWords(string sentence)
+        {
+            string result = "";
+            string bufer = "";
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                
+                if (sentence[i] == ' ')
+                {
+                    result = result + ReverseStringMoreThan(bufer, 5);
+
+                    result = result + " ";
+                    bufer = "";
+                }
+                else 
+                    bufer = bufer + sentence[i];
+
+                if (i == sentence.Length - 1 && (bufer.Length != 0))
+                {
+                    result = result + ReverseStringMoreThan(bufer, 5);
+                }
+            }
+            return result;
+        }
+
+        public string ReverseStringMoreThan(string str, int i)
+        {
+            string result = "";
+            if (str.Length >= i)
+            {
+                result = ReverseString(str);
+            }
+            else
+            {
+                result = str;
+            }
+
+            return result;
+        }
+
+        public string ReverseString(string str)
+        {
+            string result = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                result = result + str[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ TestProject/Stop_gninnipS_My_sdroW_Test.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Stop_gninnipS_My_sdroW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    class Stop_gninnipS_My_sdroW_Test
+    {
+        [Test]
+        public void IsStringMoreThen5wordSymbolsReversed()
+        {
+            Assert.AreEqual("emocleW", new Kata().SpinWords("Welcome"));
+            Assert.AreEqual("Hey wollef sroirraw", new Kata().SpinWords("Hey fellow warriors"));
+            Assert.AreEqual("This is a test", new Kata().SpinWords("This is a test"));
+            Assert.AreEqual("This is rehtona test", new Kata().SpinWords("This is another test"));
+            Assert.AreEqual("You are tsomla to the last test", new Kata().SpinWords("You are almost to the last test"));
+            Assert.AreEqual("Just gniddik ereht is llits one more", new Kata().SpinWords("Just kidding there is still one more"));
+        }
+    }
+}
